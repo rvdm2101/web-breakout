cb571c9ffd287dc57b5e1ff111ced9ade468d7da
rvdm2101 <rvdmolen2101@gmail.com>
2023-03-02 08:42:19 +0100

    chore(wb-54): Added todo

--- types/type.d.ts
+++ types/type.d.ts
@@ -1,3 +1,4 @@
 type TGenerateGame = (elementSelector: string) => void;
 type THitAndBounce = TBounce | false;
+// @TODO (WB-54): turn direction into enum
 type TBounce = { amount: number; direction: "x" | "y" };
